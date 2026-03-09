da3fd41e44a1f60bbc025a29156efe9e5535865a
scudch <scudch@U-602L6CQ4-0031.local>
2026-03-09 15:54:28 +0800

    tuner_type -> full, dataset -> modelscope/wikitext#1000, change output dir

--- Moonlight-16B-A3B-Instruct/sft.sh
+++ Moonlight-16B-A3B-Instruct/sft.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 swift sft \
 --config Moonlight-16B-A3B-Instruct/sft.yaml \
---output_dir /mnt/exp/moe-lite/jobs/Moonlight-16B-A3B-Instruct
+--output_dir /mnt/exp/moe-lite/jobs/Moonlight-16B-A3B-Instruct/test_1
