7c54a44eb0a6099dca64abd6a3482c3c3a9cafae
scudch <scudch@U-602L6CQ4-0031.local>
2026-03-09 09:38:14 +0800

    change root in config

--- examples/yaml/sft.sh
+++ examples/yaml/sft.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 swift sft \
---config sft.yaml \
---output_dir /mnt/exp/moe-lite/jobs/
+--config examples/yaml/sft.yaml \
+--output_dir /mnt/exp/moe-lite/jobs/Qwen3-4B-Instruct-2507
