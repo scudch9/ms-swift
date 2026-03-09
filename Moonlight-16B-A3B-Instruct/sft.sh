8f85489efa5ee979c1fcfc7ac2cdd5a0fa08f4f8
scudch <scudch@U-602L6CQ4-0031.local>
2026-03-09 20:12:39 +0800

    dist

--- Moonlight-16B-A3B-Instruct/sft.sh
+++ Moonlight-16B-A3B-Instruct/sft.sh
@@ -1,3 +1,7 @@
-CUDA_VISIBLE_DEVICES=0 swift sft \
+nproc_per_node=8
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+NPROC_PER_NODE=$nproc_per_node \
+swift sft \
 --config Moonlight-16B-A3B-Instruct/sft.yaml \
 --output_dir /mnt/exp/moe-lite/jobs/Moonlight-16B-A3B-Instruct/test_1
